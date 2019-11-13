0d9aa70f28dbe2e5f6e5cbbd877843ce66a3ccbb
virgiliomsilva <44083872+virgiliomsilva@users.noreply.github.com>
2019-11-13 19:58:18 +0000

    shotCaller organization

--- driftsExtract.m
+++ driftsExtract.m
@@ -1,15 +1,10 @@
-function [ISD, notConverged, means, PoE] = driftsExtract(buildingName, code, noFloors, floorHeight, IML, ISDthreshold, print, stoGlo)
-% buildingName = 'regular';
-% code = 'EC2' ;
-% noFloors = 5;
-% height = 2.8;
-% IML = [.05, .1, .3, .5, .75, 1, 1.25, 1.5, 1.75, 2];
+function [ISD, notConverged, means, PoE] = driftsExtract(dir, buildingName, code, noFloors, floorHeight, IML, ISDthreshold, print, stoGlo)
 %% IMPORT ALL DRIFTS | EACH FLOOR PER PAGE
-for i = 1 : noFloors
-    disp_x_in = importdata(['input\disp_' buildingName '_' code '_x_' num2str(i) '.txt']);
+for i = [1 10]% 1 : noFloors
+    disp_x_in = importdata([dir '\disp_' buildingName '_' code '_x_' num2str(i) '.txt']);
     disp_x (:,:,i) = disp_x_in.data ;
     clear disp_x_in
-    disp_y_in = importdata(['input\disp_' buildingName '_' code '_y_' num2str(i) '.txt']);
+    disp_y_in = importdata([dir '\disp_' buildingName '_' code '_y_' num2str(i) '.txt']);
     disp_y (:,:,i) = disp_y_in.data ;
     clear disp_y_in
 end
@@ -24,8 +19,10 @@ for i = 2 : 2 : timeNrecs
     for k = 1 : recordLength
         switch stoGlo
             case 'local'
-                interStoryDrift(k,2) = sqrt(disp_x(k, i, 1) ^ 2 + disp_y(k, i, 1) ^ 2);%ground floor
-                for j = 2 : noFloors %other floors
+                %ground floor
+                interStoryDrift(k,2) = sqrt(disp_x(k, i, 1) ^ 2 + disp_y(k, i, 1) ^ 2);
+                %other floors
+                for j = 2 : noFloors 
                     dif_x = disp_x(k, i, j - 1) - disp_x(k, i, j);
                     dif_y = disp_y(k, i, j - 1) - disp_y(k, i, j);
                     interStoryDrift(k, j+1) = sqrt(dif_x ^2 + dif_y ^2);
@@ -52,17 +49,21 @@ end
 
 clear dif_x dif_y disp_y height interStoryDrift j k maxISDrift noFloors noIML ...
     recsPerIntensity timeNrecs
-%% DELETE THE ONES THAT DIDN'T CONVERGED
+%% THE ONES THAT DIDN'T CONVERGED aka also dead give it a value to make it exceed!
+notConverged = [];
 for i = 1 : noRecs
     nSeismicRec = importdata(sprintf('records_info\\rec_%d_dir1.txt',i));
     seismoRecSize = size(nSeismicRec,1);
     recordLength = sum(~isnan(disp_x(:, 2*i,1)));
-    if (seismoRecSize - 10) > recordLength
-        ISD(ISD(:,2) == i, :) = [];
+    if (seismoRecSize - 10) > recordLength %|| max(abs(disp_x(:,2*i,10))) >
+%         ISD(ISD(:,2) == i, :) = [];
+        ISD(ISD(:,2) == i, 3) = 2 * ISDthreshold;
+        notConverged = [notConverged, i];
     end
 end
 
-notConverged = setxor([1:noRecs]', ISD(:,2));
+% notConverged = setxor([1:noRecs]', ISD(:,2));
+
 ISD = sortrows(ISD, [1 3]);
 clear disp_x noRecs nSeismicRec recordLength seismoRecSize
 %% CALCULATE THE PROBABILITY OF EXCEDENCE
@@ -77,15 +78,15 @@ for i = 1 : length(IML)
     means(i, 2) = mean(ISD(ISD(:,1) == IML(i),3));
 end
 
-hold on
-scatter(ISD(:,1), ISD(:,3), 'b');
-scatter(means(:,1), means(:,2), 'filled','o r');
-xticks (IML);
-title([upper(buildingName(1)) lower(buildingName(2:end)) ' ' code]);
-xlabel('IML');
-ylabel('ISD');
-switch print
-    case 'print'
-%         print([upper(buildingName(1)), lower(buildingName(2:end)), ' ', code],'-dpng');
-end
-hold off
+% hold on
+% scatter(ISD(:,1), ISD(:,3), 'b');
+% scatter(means(:,1), means(:,2), 'filled','o r');
+% xticks (IML);
+% title([upper(buildingName(1)) lower(buildingName(2:end)) ' ' code]);
+% xlabel('IML');
+% ylabel('ISD');
+% switch print
+%     case 'print'
+% %         print([upper(buildingName(1)), lower(buildingName(2:end)), ' ', code],'-dpng');
+% end
+% hold off
--- shotCaller.m
+++ shotCaller.m
@@ -1,31 +1,91 @@
-% example caller
+% caller
 clear
 clc
+
 tic
-buildingName = 'regular';
-code = 'DC2' ; 
-noFloors = 5;
-height = 2.8;
+
+noFloors = 10;
+heightFloor = 28.5/noFloors;
 IML = [.05, .1, .3, .5, .75, 1, 1.25, 1.5, 1.75, 2];
-ISDthreshold = .32/14;%.25/14;   % .25/14; %0.0407535714285714;   %0.0067;
-%%
-[ISDmatrix, notConvergedRecords, IMLisd, PoE] = driftsExtract(buildingName, code, noFloors, height, IML, ISDthreshold,'print','global');
+hazard_curve = importdata('hazardCurveLis.mat');
 
-hazard_curve = importdata('hazardCurve.mat');
-vul_coll_curve = PoE;
-rtP = 50;
+for i = 2%1 : 3%: 6
+    switch i
+        case 1
+            disp80 = 0.75;
+            buildingName = 'tenerPT' ;
+            code = 'DC1' ; 
+            dir = ['D:\Google Drive\Disco UPorto\MIECIVIL\5 Ano\2 Semestre\Models\tener\Portugal\ite2\' code '\seismoStruct\DTHA_results'];
 
-[aal_aapc] = aal_aapc_calc(hazard_curve,vul_coll_curve,rtP);
-%%
+        case 2
+            disp80 = 1.15;
+            buildingName = 'tenerPT' ;
+            code = 'DC2' ; 
+            dir = ['D:\Google Drive\Disco UPorto\MIECIVIL\5 Ano\2 Semestre\Models\tener\Portugal\ite2\' code '\seismoStruct\DTHA_results'];
+            
+        case 3
+            disp80 = 1.19;
+            buildingName = 'tenerPT' ;
+            code = 'DC3' ; 
+            dir = ['D:\Google Drive\Disco UPorto\MIECIVIL\5 Ano\2 Semestre\Models\tener\Portugal\ite2\' code '\seismoStruct\DTHA_results'];
 
-X = PoE(:,1);
-Y = PoE(:,2);
-func = @(fit,xdata)logncdf(xdata,fit(1),fit(2));
-fit = lsqcurvefit(func,[0.8 0.4],X,Y);
-figure();
-plot(linspace(min(X),max(X),100),logncdf(linspace(min(X),max(X),100),fit(1),fit(2)),'b');
-hold on 
-plot(X,Y,'o','Color',[0.5 0.5 0.5]);
+        case 4
+            disp80 = 0.54;
+            buildingName = 'tenerIT' ;
+            code = 'DC1' ; 
+            dir = ['D:\Google Drive\Disco UPorto\MIECIVIL\5 Ano\2 Semestre\Models\tener\Italy\ite1\' code '\seismoStruct\DTHA_results'];
 
-save(['fit_' buildingName '_' code],'fit');
-toc
+        case 5
+            disp80 = 0.63;
+            buildingName = 'tenerIT' ;
+            code = 'DC2' ; 
+            dir = ['D:\Google Drive\Disco UPorto\MIECIVIL\5 Ano\2 Semestre\Models\tener\Italy\ite1\' code '\seismoStruct\DTHA_results'];
+
+        case 6
+            disp80 = 1.00;
+            buildingName = 'tenerIT' ;
+            code = 'DC3' ; 
+            dir = ['D:\Google Drive\Disco UPorto\MIECIVIL\5 Ano\2 Semestre\Models\tener\Italy\ite1\' code '\seismoStruct\DTHA_results'];
+    end
+    disp(['Currently executing ' buildingName ' ' code])
+    disp('...')
+    
+    ISDthreshold = disp80/(heightFloor * noFloors);
+    [ISDmatrix, notConvergedRecords, IMLisd, PoE] = driftsExtract(dir, buildingName, code, noFloors, heightFloor, IML, ISDthreshold,'print','global');
+
+    
+    vul_coll_curve = PoE;
+    rtP = 50;
+    [aal_aapc] = aal_aapc_calc(hazard_curve,vul_coll_curve,rtP);
+    save([buildingName '_' code '_aapc'],'aal_aapc');
+
+    % plot 1 
+    subplot(1,2,1)
+    
+    hold on
+    scatter(ISDmatrix(:,1), ISDmatrix(:,3), 'b');
+    scatter(IMLisd(:,1), IMLisd(:,2), 'filled','o r');
+    xticks (IML);
+    title([buildingName ' ' code]);
+    xlabel('IML');
+    ylabel('ISD');
+    hold off
+    
+    % plot 2 
+    subplot(1,2,2)
+    X = PoE(:,1);
+    Y = PoE(:,2);
+    func = @(fit,xdata)logncdf(xdata,fit(1),fit(2));
+    fit = lsqcurvefit(func,[0.8 0.4],X,Y);
+    save([buildingName '_' code '_fit'],'fit');
+%     figure();
+    plot(linspace(min(X),max(X),100),logncdf(linspace(min(X),max(X),100),fit(1),fit(2)),'b');
+    hold on
+    plot(X,Y,'o','Color',[0.5 0.5 0.5]);
+    hold off
+        
+    saveas(gcf,[buildingName '_' code '.png'])
+    
+    
+    toc
+end
